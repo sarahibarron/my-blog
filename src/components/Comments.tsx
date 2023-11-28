e2d914cf8b03342d0626d6f0da41161ca01d4526
SARAH BARRON <sarahibarron@hotmail.co.uk>
2023-11-28 13:48:33 +0000

    first

---
+++ src/components/Comments.tsx
@@ -0,0 +1,18 @@
+export default async function Comments(postSlug) {
+  return (
+    <div>
+      <h2>| Comments |</h2>
+      <h3>Leave a comment: </h3>
+
+      <form action={`/api/comments/${postSlug}`} method="POST">
+        <label htmlFor="username">Name:</label>
+        <input type="text" name="username" />
+
+        <label htmlFor="comment">Your comment::</label>
+        <textarea name="username" cols={30} rows={10} />
+
+        <button type="submit">send comment</button>
+      </form>
+    </div>
+  );
+}
